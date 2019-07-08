d1d58d2d7fedc869e3c16ce3d4e2aaac73e6c499
dekisa <dejan.95.lukic@gmail.com>
2019-07-08 16:05:34 +0200

    removed build files

--- build/src/CMakeFiles/cgra-me_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libcgra-me_static.pdb"
-  "../lib/libcgra-me_static.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/cgra-me_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/thirdparty/scipoptsuite/CMakeFiles/scipoptsuite.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/scipoptsuite"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/scipoptsuite.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
