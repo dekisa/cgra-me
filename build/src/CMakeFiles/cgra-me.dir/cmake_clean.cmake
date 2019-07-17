c458152dc030b05071e9f0a6091b3a17229a8037
dekisa <dejan.95.lukic@gmail.com>
2019-07-17 18:49:04 +0200

    removed build files

--- build/src/CMakeFiles/cgra-me.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libcgra-me.pdb"
-  "../lib/libcgra-me.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/cgra-me.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/modules/CMakeFiles/modules.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/modules.dir/SimpleArchFU.cpp.o"
-  "CMakeFiles/modules.dir/AdresPE.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/modules.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
