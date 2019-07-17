243d60a9142b8091887efb1e78dbf027f1fe80dc
dekisa <dejan.95.lukic@gmail.com>
2019-07-17 19:20:39 +0200

    added test folders

--- output/context-div.js
+++
@@ -1,6 +0,0 @@
-document.write("\
-    <div class=\"tab\">\
-        <button class=\"tablinks\" onclick=\"showContext(event, 'context_0')\" id=\"defaultOpen\">Context 0</button>\
-    </div>\
-    <div id=\"context_0\" class=\"tabcontent\"></div>\
-");
